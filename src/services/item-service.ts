7cd223a82550950007291b81460127e1ba0d2188
Daniel Radtke <danrad7@gmail.com>
2025-09-20 03:29:06 -0700

    feat: another possible

--- src/services/item-service.ts
+++ src/services/item-service.ts
@@ -1,40 +1,28 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { DOCUMENT } from '@angular/common';
-import { catchError, expand, map, Observable, of, reduce, takeWhile } from 'rxjs';
+import { catchError, expand, of, reduce, takeWhile } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ItemService {
-  constructor(
-    private http: HttpClient,
-    @Inject(DOCUMENT) private document: Document
-  ) {}
+  private basePath = document.querySelector('base')?.getAttribute('href') || '/';
 
-  private getBaseHref(): string {
-    const base = this.document.getElementsByTagName('base')[0].getAttribute('href') || '/';
-    return base.endsWith('/') ? base : base + '/';
+  constructor(private http: HttpClient) {}
+
+  getJsonFile<T = any>(filename: string) {
+    return this.http.get<T>(`${this.basePath}data/${filename}`);
   }
 
-  getJsonFile<T = any>(filename: string): Observable<T> {
-    const baseHref = this.getBaseHref();
-    return this.http.get<T>(`${baseHref}data/${filename}`);
-  }
-
-  loadAllChunks<T>(baseName: string): Observable<T[]> {
-    const baseHref = this.getBaseHref();
+  loadAllChunks<T>(baseName: string) {
     let index = 1;
     return of(null).pipe(
       expand(() => {
         const fileName =
           index === 1 ? `${baseName}_data.json` : `${baseName}_data${index}.json`;
-        return this.http.get<T[]>(`${baseHref}data/${fileName}`).pipe(
-          catchError(() => of(null)),
-          map((data) => {
-            index++;
-            return data;
-          })
+        index++;
+        return this.http.get<T[]>(`${this.basePath}data/${fileName}`).pipe(
+          catchError(() => of(null))
         );
       }),
       takeWhile((data) => data !== null),
